4d3fc45ff86839462d8a838b64cbfeb58f9dcc94
Hartog C. de Mik <hcdm@matchvertise.com>
2011-03-21 12:03:49 +0100

    Tests for casting dates and times

--- test/app/models/post.rb
+++ test/app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   has_eav :through => :post_attribute do
     eav_attribute :author_name
-    eav_attribute :author_email    
+    eav_attribute :author_email
+    eav_attribute :published_on, Date
+    eav_attribute :last_update, Time
   end
 end
